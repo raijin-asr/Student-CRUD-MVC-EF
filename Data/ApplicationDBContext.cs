604adcdba47aad9ec2c6d7dbbd67161ff2d81b13
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2025-05-27 00:46:22 +0545

    Day 1: Create, Setup Project, Database connection

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_CRUD_MVC_EF.Models.Entities;
+
+namespace Student_CRUD_MVC_EF.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Student-CRUD-MVC-EF
+
 CRUD operation on Student using MVC EF Core Dotnet C#
+
+# Things i've learned and implemented(day wise)
+
+    Day 1: Create, Setup Project, Database connection
